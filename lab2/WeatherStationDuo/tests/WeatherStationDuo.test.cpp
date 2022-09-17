6dda5b44e081d6698ee9a92b5fd77e23d4cc8e14
m3tro1d <qazwf734@gmail.com>
2022-09-17 22:09:05 +0300

    lab2 WeatherStationDuo: add tests

---
+++ lab2/WeatherStationDuo/tests/WeatherStationDuo.test.cpp
@@ -0,0 +1,104 @@
+#define CATCH_CONFIG_MAIN
+#include "../WeatherData/CWeatherData.h"
+#include "catch.hpp"
+#include <sstream>
+
+TEST_CASE("several observables in the same observers")
+{
+	GIVEN("a few observables")
+	{
+		CWeatherData observable1;
+		CWeatherData observable2;
+
+		AND_GIVEN("an observer")
+		{
+			class CDummyObserver : public IObserver<WeatherInfo>
+			{
+			public:
+				explicit CDummyObserver(std::ostream& out)
+					: m_out(out)
+				{
+				}
+
+				void SetObservableName(IObservable<WeatherInfo>& observable, std::string const& name) override
+				{
+					m_observableNames[&observable] = name;
+				}
+
+				void RemoveObservable(IObservable<WeatherInfo>& observable) override
+				{
+					m_observableNames.erase(&observable);
+				}
+
+			private:
+				void Update(IObservable<WeatherInfo>& observable, WeatherInfo const& data) override
+				{
+					auto name = m_observableNames.find(&observable);
+					if (name != m_observableNames.end())
+					{
+						m_out << name->second;
+					}
+					else
+					{
+						m_out << "unknown";
+					}
+
+					m_out << '\n';
+				}
+
+				std::map<IObservable<WeatherInfo>*, std::string> m_observableNames;
+				std::ostream& m_out;
+			};
+
+			std::ostringstream out;
+			CDummyObserver observer(out);
+
+			observable1.RegisterObserver(observer);
+			observable2.RegisterObserver(observer);
+
+			WHEN("notifying from both observables")
+			{
+				observable1.NotifyObservers();
+				observable2.NotifyObservers();
+
+				THEN("both of them are unknown")
+				{
+					REQUIRE(out.str() == "unknown\nunknown\n");
+				}
+			}
+
+			WHEN("settings names for observables")
+			{
+				observer.SetObservableName(observable1, "1");
+				observer.SetObservableName(observable2, "2");
+
+				AND_WHEN("notifying from both observables")
+				{
+					observable1.NotifyObservers();
+					observable2.NotifyObservers();
+
+					THEN("they have different names")
+					{
+						REQUIRE(out.str() == "1\n2\n");
+					}
+				}
+
+				AND_WHEN("removing info about first observable")
+				{
+					observer.RemoveObservable(observable1);
+
+					AND_WHEN("notifying from both observables")
+					{
+						observable1.NotifyObservers();
+						observable2.NotifyObservers();
+
+						THEN("first one is unknown")
+						{
+							REQUIRE(out.str() == "unknown\n2\n");
+						}
+					}
+				}
+			}
+		}
+	}
+}
