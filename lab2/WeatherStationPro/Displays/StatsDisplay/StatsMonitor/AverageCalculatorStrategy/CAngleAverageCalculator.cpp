6f3375bce3a1d9076293176f150011ed59d3947d
m3tro1d <qazwf734@gmail.com>
2022-09-18 10:51:11 +0300

    lab2 WeatherStationPro: add average angle calculation

--- lab2/WeatherStationPro/Displays/StatsDisplay/StatsMonitor/AverageCalculatorStrategy/CAngleAverageCalculator.cpp
+++ lab2/WeatherStationPro/Displays/StatsDisplay/StatsMonitor/AverageCalculatorStrategy/CAngleAverageCalculator.cpp
@@ -1,12 +1,25 @@
 #include "CAngleAverageCalculator.h"
+#include <cmath>
 
 void CAngleAverageCalculator::Update(double value)
 {
-	// TODO
+	auto angle = DegreesToRadians(value);
+	m_sineSum += std::sin(angle);
+	m_cosineSum += std::cos(angle);
+	++m_accumulatedCount;
 }
 
 double CAngleAverageCalculator::GetAverage() const
 {
-	// TODO
-	return 0;
+	return RadiansToDegrees(std::atan2(m_sineSum / m_accumulatedCount, m_cosineSum / m_accumulatedCount));
+}
+
+double CAngleAverageCalculator::DegreesToRadians(double value)
+{
+	return value * M_PI / 180;
+}
+
+double CAngleAverageCalculator::RadiansToDegrees(double value)
+{
+	return value * 180 / M_PI;
 }
