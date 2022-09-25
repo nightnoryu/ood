6fc993f3c9bb86d7ec031b9bdcdcd333d36b254f
m3tro1d <qazwf734@gmail.com>
2022-09-25 21:42:33 +0300

    lab2 WeatherStationDuoSignals: implement observers removal

--- lab2/WeatherStationDuoSignals/Observer/CObservable.hpp
+++ lab2/WeatherStationDuoSignals/Observer/CObservable.hpp
@@ -11,7 +11,10 @@ public:
 
 	void RegisterObserver(ObserverType& observer) override
 	{
-		m_signal.connect(boost::bind(&ObserverType::Update, &observer, _1, _2));
+		// TODO prioritize observers with groups
+		// https://www.boost.org/doc/libs/1_80_0/doc/html/signals2/tutorial.html#id-1.3.35.4.4.3
+		auto connection = m_signal.connect(boost::bind(&ObserverType::Update, &observer, _1, _2));
+		m_connections.emplace(&observer, connection);
 	}
 
 	void NotifyObservers() override
@@ -22,7 +25,11 @@ public:
 
 	void RemoveObserver(ObserverType& observer) override
 	{
-		// TODO
+		auto it = m_connections.find(&observer);
+		if (it != m_connections.end())
+		{
+			it->second.disconnect();
+		}
 	}
 
 protected:
@@ -32,4 +39,5 @@ private:
 	using SignalType = boost::signals2::signal<void(IObservable<T>&, T const&)>;
 
 	SignalType m_signal;
+	std::unordered_map<ObserverType*, boost::signals2::connection> m_connections;
 };
