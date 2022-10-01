feb0677b955c93cd8b8f74d94d020cce5f26bb41
m3tro1d <qazwf734@gmail.com>
2022-10-01 09:55:54 +0300

    lab3 Transform: fix error detection for files

--- lab3/Transform/OutputDataStream/FileOutputStream/CFileOutputStream.cpp
+++ lab3/Transform/OutputDataStream/FileOutputStream/CFileOutputStream.cpp
@@ -7,24 +7,16 @@ CFileOutputStream::CFileOutputStream(std::string const& filename)
 
 void CFileOutputStream::WriteByte(std::uint8_t data)
 {
-	try
+	if (!m_file.write(reinterpret_cast<char const*>(&data), 1))
 	{
-		m_file.write(reinterpret_cast<char const*>(&data), 1);
-	}
-	catch (std::exception&)
-	{
-		throw std::ios_base::failure("failed to write byte");
+		throw std::ios_base::failure("failed to write to file");
 	}
 }
 
 void CFileOutputStream::WriteBlock(void const* srcData, std::streamsize size)
 {
-	try
+	if (!m_file.write(static_cast<char const*>(srcData), size))
 	{
-		m_file.write(static_cast<char const*>(srcData), size);
-	}
-	catch (std::exception&)
-	{
-		throw std::ios_base::failure("failed to write block");
+		throw std::ios_base::failure("failed to write to file");
 	}
 }
