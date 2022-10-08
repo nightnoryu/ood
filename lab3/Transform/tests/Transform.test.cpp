ff16a89ea3fde9fbeb24e73dfa3f56403b8fe5ca
m3tro1d <qazwf734@gmail.com>
2022-10-08 20:27:44 +0300

    lab3 Transform: add tests template

---
+++ lab3/Transform/tests/Transform.test.cpp
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+SCENARIO("Memory input stream")
+{
+}
+
+SCENARIO("Memory output stream")
+{
+}
+
+SCENARIO("Encrypting output stream decorator")
+{
+}
+
+SCENARIO("Decrypting input stream decorator")
+{
+}
+
+SCENARIO("Compressing output stream decorator")
+{
+}
+
+SCENARIO("Decompressing input stream decorator")
+{
+}
