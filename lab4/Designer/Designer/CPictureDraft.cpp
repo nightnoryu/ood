96500cc0f0326526c69e270ebcb8dc4fb2f98e87
m3tro1d <qazwf734@gmail.com>
2022-10-09 21:43:18 +0300

    lab4 Designer: remove redundant functions

--- lab4/Designer/Designer/CPictureDraft.cpp
+++ lab4/Designer/Designer/CPictureDraft.cpp
@@ -5,6 +5,11 @@ bool CPictureDraft::IsEmpty() const
 	return m_shapes.empty();
 }
 
+void CPictureDraft::AddShape(CShapePtr&& shape)
+{
+	m_shapes.push_back(std::move(shape));
+}
+
 CPictureDraft::ConstIterator CPictureDraft::begin() const
 {
 	return m_shapes.begin();
@@ -13,19 +18,4 @@ CPictureDraft::ConstIterator CPictureDraft::begin() const
 CPictureDraft::ConstIterator CPictureDraft::end() const
 {
 	return m_shapes.end();
-}
-
-void CPictureDraft::AddShape(CShapePtr&& shape)
-{
-	m_shapes.push_back(std::move(shape));
-}
-
-CPictureDraft::ConstIterator begin(CPictureDraft const& draft)
-{
-	return draft.begin();
-}
-
-CPictureDraft::ConstIterator end(CPictureDraft const& draft)
-{
-	return draft.end();
 }
