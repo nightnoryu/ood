aa0c6b2c84ad07096e78a09273bd4babf6875368
m3tro1d <qazwf734@gmail.com>
2022-10-09 21:47:34 +0300

    lab4 Designer: add canvas stub

---
+++ lab4/Designer/Canvas/CCanvas.cpp
@@ -0,0 +1,16 @@
+#include "CCanvas.h"
+
+void CCanvas::SetColor(Color color)
+{
+	// TODO
+}
+
+void CCanvas::DrawLine(const Point& from, const Point& to)
+{
+	// TODO
+}
+
+void CCanvas::DrawEllipse(const Point& center, int width, int height)
+{
+	// TODO
+}
