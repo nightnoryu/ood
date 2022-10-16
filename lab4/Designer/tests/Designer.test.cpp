7979c8ed387064ca56546074703afd57b6e3469a
m3tro1d <qazwf734@gmail.com>
2022-10-16 10:31:48 +0300

    lab4 Designer: add tests template

---
+++ lab4/Designer/tests/Designer.test.cpp
@@ -0,0 +1,42 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "fakeit.hpp"
+
+TEST_CASE("shapes")
+{
+	SECTION("ellipse")
+	{
+	}
+
+	SECTION("rectangle")
+	{
+	}
+
+	SECTION("regular polygon")
+	{
+	}
+
+	SECTION("triangle")
+	{
+	}
+}
+
+TEST_CASE("shape factory")
+{
+}
+
+TEST_CASE("picture draft")
+{
+}
+
+TEST_CASE("designer")
+{
+}
+
+TEST_CASE("painter")
+{
+}
+
+TEST_CASE("canvas")
+{
+}
