44a25b0b6b87064103e6f0e05e1d43560d210b91
m3tro1d <qazwf734@gmail.com>
2022-10-20 15:05:26 +0300

    lab5 Editor: add tests outline

---
+++ lab5/Editor/tests/Editor.test.cpp
@@ -0,0 +1,25 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("document model")
+{
+	SECTION("document")
+	{
+	}
+
+	SECTION("image")
+	{
+	}
+
+	SECTION("paragraph")
+	{
+	}
+
+	SECTION("document item")
+	{
+	}
+}
+
+TEST_CASE("commands")
+{
+}
