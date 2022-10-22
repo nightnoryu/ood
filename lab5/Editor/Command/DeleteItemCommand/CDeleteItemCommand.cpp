ccfe16f15f976c4b1adab8ef8211369ebccbdc7b
m3tro1d <qazwf734@gmail.com>
2022-10-22 21:13:44 +0300

    lab5 Editor: fix accessing element before range check

--- lab5/Editor/Command/DeleteItemCommand/CDeleteItemCommand.cpp
+++ lab5/Editor/Command/DeleteItemCommand/CDeleteItemCommand.cpp
@@ -3,12 +3,13 @@
 CDeleteItemCommand::CDeleteItemCommand(std::vector<CDocumentItem>& items, std::size_t index)
 	: m_items(items)
 	, m_index(index)
-	, m_deletedItem(std::make_unique<CDocumentItem>(m_items.at(index)))
 {
 	if (m_index >= items.size())
 	{
 		throw std::invalid_argument("invalid index");
 	}
+
+	m_deletedItem = std::make_unique<CDocumentItem>(m_items.at(index));
 }
 
 void CDeleteItemCommand::DoExecute()
