695bccc2a79a2c2cd4acd51cc5f5b1f217ec60a9
m3tro1d <qazwf734@gmail.com>
2022-10-22 20:26:11 +0300

    lab5 Editor: adjust tests for using history

--- lab5/Editor/tests/Editor.test.cpp
+++ lab5/Editor/tests/Editor.test.cpp
@@ -8,21 +8,21 @@
 
 TEST_CASE("document model")
 {
+	fakeit::Mock<IHistory> historyMock;
+	fakeit::Fake(Method(historyMock, CanUndo));
+	fakeit::Fake(Method(historyMock, Undo));
+	fakeit::Fake(Method(historyMock, CanRedo));
+	fakeit::Fake(Method(historyMock, Redo));
+	fakeit::Fake(Method(historyMock, AddAndExecuteCommand));
+
+	fakeit::Mock<ISaver> saverMock;
+	fakeit::Fake(Method(saverMock, Save));
+
+	auto& history = historyMock.get();
+	auto& saver = saverMock.get();
+
 	SECTION("document")
 	{
-		fakeit::Mock<IHistory> historyMock;
-		fakeit::Fake(Method(historyMock, CanUndo));
-		fakeit::Fake(Method(historyMock, Undo));
-		fakeit::Fake(Method(historyMock, CanRedo));
-		fakeit::Fake(Method(historyMock, Redo));
-		fakeit::Fake(Method(historyMock, AddAndExecuteCommand));
-
-		fakeit::Mock<ISaver> saverMock;
-		fakeit::Fake(Method(saverMock, Save));
-
-		auto& history = historyMock.get();
-		auto& saver = saverMock.get();
-
 		GIVEN("a document")
 		{
 			CDocument document(history, saver);
@@ -106,11 +106,11 @@ TEST_CASE("document model")
 		{
 			THEN("it throws an exception")
 			{
-				REQUIRE_THROWS_AS(CImage(path, 0, 10), std::invalid_argument);
-				REQUIRE_THROWS_AS(CImage(path, 10, 0), std::invalid_argument);
+				REQUIRE_THROWS_AS(CImage(path, 0, 10, history), std::invalid_argument);
+				REQUIRE_THROWS_AS(CImage(path, 10, 0, history), std::invalid_argument);
 
-				REQUIRE_THROWS_AS(CImage(path, 5001, 10), std::invalid_argument);
-				REQUIRE_THROWS_AS(CImage(path, 10, 5001), std::invalid_argument);
+				REQUIRE_THROWS_AS(CImage(path, 5001, 10, history), std::invalid_argument);
+				REQUIRE_THROWS_AS(CImage(path, 10, 5001, history), std::invalid_argument);
 			}
 		}
 
@@ -118,11 +118,11 @@ TEST_CASE("document model")
 		{
 			THEN("it is created just fine")
 			{
-				REQUIRE_NOTHROW(CImage(path, 1, 10));
-				REQUIRE_NOTHROW(CImage(path, 10, 1));
+				REQUIRE_NOTHROW(CImage(path, 1, 10, history));
+				REQUIRE_NOTHROW(CImage(path, 10, 1, history));
 
-				REQUIRE_NOTHROW(CImage(path, 5000, 10));
-				REQUIRE_NOTHROW(CImage(path, 10, 5000));
+				REQUIRE_NOTHROW(CImage(path, 5000, 10, history));
+				REQUIRE_NOTHROW(CImage(path, 10, 5000, history));
 			}
 		}
 
@@ -130,7 +130,7 @@ TEST_CASE("document model")
 		{
 			THEN("it throws an exception")
 			{
-				REQUIRE_THROWS_AS(CImage("test.txt", 10, 10), std::invalid_argument);
+				REQUIRE_THROWS_AS(CImage("test.txt", 10, 10, history), std::invalid_argument);
 			}
 		}
 
@@ -138,7 +138,7 @@ TEST_CASE("document model")
 		{
 			THEN("it throws an exception")
 			{
-				REQUIRE_THROWS_AS(CImage("non_existing.jpg", 10, 10), std::runtime_error);
+				REQUIRE_THROWS_AS(CImage("non_existing.jpg", 10, 10, history), std::runtime_error);
 			}
 		}
 
@@ -149,7 +149,7 @@ TEST_CASE("document model")
 			{
 				std::size_t const width = 50;
 				std::size_t const height = 90;
-				CImage image(path, width, height);
+				CImage image(path, width, height, history);
 				tmpPath = image.GetPath();
 
 				THEN("parameters match")
@@ -164,28 +164,13 @@ TEST_CASE("document model")
 					REQUIRE(std::filesystem::exists(tmpPath));
 				}
 
-				AND_WHEN("trying to resize with invalid dimensions")
+				WHEN("resizing the image")
 				{
-					THEN("it throws an exception")
+					image.Resize(10, 10);
+
+					THEN("it delegates to history")
 					{
-						REQUIRE_THROWS_AS(image.Resize(0, 10), std::invalid_argument);
-						REQUIRE_THROWS_AS(image.Resize(10, 0), std::invalid_argument);
-
-						REQUIRE_THROWS_AS(image.Resize(5001, 10), std::invalid_argument);
-						REQUIRE_THROWS_AS(image.Resize(10, 5001), std::invalid_argument);
-					}
-				}
-
-				WHEN("trying to resize with borderline dimensions")
-				{
-					std::size_t const newWidth = 1;
-					std::size_t const newHeight = 5000;
-					REQUIRE_NOTHROW(image.Resize(newWidth, newHeight));
-
-					THEN("it is resized just fine")
-					{
-						REQUIRE(image.GetWidth() == newWidth);
-						REQUIRE(image.GetHeight() == newHeight);
+						fakeit::Verify(Method(historyMock, AddAndExecuteCommand)).Exactly(1);
 					}
 				}
 			}
@@ -205,7 +190,7 @@ TEST_CASE("document model")
 		WHEN("creating a paragraph")
 		{
 			std::string const text = "boobz";
-			CParagraph paragraph(text);
+			CParagraph paragraph(text, history);
 
 			THEN("it has initial text")
 			{
@@ -214,12 +199,11 @@ TEST_CASE("document model")
 
 			AND_WHEN("changing the text")
 			{
-				std::string const newText = "NO BOOBZ :(";
-				paragraph.SetText(newText);
+				paragraph.SetText("NO BOOBZ D:");
 
-				THEN("it is changed")
+				THEN("it delegates to history")
 				{
-					REQUIRE(paragraph.GetText() == newText);
+					fakeit::Verify(Method(historyMock, AddAndExecuteCommand)).Exactly(1);
 				}
 			}
 		}
